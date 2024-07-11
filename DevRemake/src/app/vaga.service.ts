d7ea1103b7de651669535da1e17bf8f9ee79d958
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-07-11 13:08:37 -0300

    Update vaga.service.ts

--- DevRemake/src/app/vaga.service.ts
+++ DevRemake/src/app/vaga.service.ts
@@ -7,9 +7,9 @@ import { Vagapreview } from './vagapreview';
   
 })
 export class VagaService {
-    url = 'http://localhost:3000/locations';
+    url = 'https://github.com/samuelgsousa/Devjobs-Remake/blob/main/DevRemake/db.json';
 
-   protected vagaPreviewList: Vagapreview[] = [
+//   protected vagaPreviewList: Vagapreview[] = [
 //     {
 //         "id": 1,
 //         "nome_empresa": "Marketing S&A",
@@ -370,7 +370,7 @@ export class VagaService {
 //                 "requisito": "Experiência em sistemas embarcados"}
 //         ]
 //     }
- ]
+// ]
 
   async getAllVagas(): Promise<Vagapreview[]>{
     const data = await fetch(this.url)
