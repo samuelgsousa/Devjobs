bea271aa84ba032cd55bd905843049ffe9d951a3
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-07-16 17:55:23 -0300

    Alterando Logo para empresas
    Adicionando sintaxe para gerar um Avatar com a inicial de cada empresa

--- DevRemake/src/app/vaga-preview/vaga-preview.component.ts
+++ DevRemake/src/app/vaga-preview/vaga-preview.component.ts
@@ -1,27 +1,32 @@
-import { Component, Injectable, Input } from '@angular/core';
+import { Component, Injectable, Input, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Vagapreview } from '../vagapreview';
 import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { VagaService } from '../vaga.service';
 
+
 @Injectable({
   providedIn: 'root',
-  
 })
 
 @Component({
   selector: 'app-vaga-preview',
   standalone: true,
   imports: [CommonModule, NgbToastModule, RouterModule, VagaPreviewComponent],
+
   template: `
+  <link rel="stylesheet" href="vaga-preview.component.scss">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
   <section class="listing">
   
   <div class="card mb-3" style="width: 30rem; height: 33rem;">
   <div class="card-header bg-transparent ">
-    <img src="https://via.placeholder.com/50" alt="Logo" class="listing-photo"/>
+
+  <div class="avatar"> {{getInitials("" + Vagapreview?.nome_empresa)}}</div>
+
+    <!-- <img src="https://via.placeholder.com/50" alt="Logo" class="listing-photo"/> -->
     <h1 class="listing-heading">{{Vagapreview?.nome_empresa}}</h1>
 
   </div>
@@ -74,6 +79,11 @@ export class VagaPreviewComponent {
    getReq = (reqList?: any): string[] => {
 return reqList?.map((req: { requisito: any; }) => req.requisito)
   }
+
+  getInitials(firstName:string) {
+    return firstName[0].toUpperCase();
+  }
+
 
   @Input() Vagapreview!: Vagapreview
   
